cfa71b5b03b6f9d34d924db0e035d363fea391de
FelipheOsorio <113398647+FelipheOsorio@users.noreply.github.com>
2023-08-25 01:44:17 -0300

    Ajustes em algumas classes, add validações

--- desafio.kt
+++ desafio.kt
@@ -1,21 +1,105 @@
-// [Template no Kotlin Playground](https://pl.kotl.in/WcteahpyN)
+enum class Nivel { BASICO, INTERMEDIARIO, AVANCADO }
 
-enum class Nivel { BASICO, INTERMEDIARIO, DIFICIL }
+class Usuario(val name: String, val cpf: String)
 
-class Usuario
-
-data class ConteudoEducacional(var nome: String, val duracao: Int = 60)
+data class ConteudoEducacional(val nome: String, var nivel: Nivel, var duracao: Int)
 
 data class Formacao(val nome: String, var conteudos: List<ConteudoEducacional>) {
 
-    val inscritos = mutableListOf<Usuario>()
-    
-    fun matricular(usuario: Usuario) {
-        TODO("Utilize o parâmetro $usuario para simular uma matrícula (usar a lista de $inscritos).")
+    private val _inscritos = mutableMapOf<String, String>()
+
+    private val inscritos: Map<String, String> = _inscritos
+
+    fun matricularAlunos(vararg listaUsuario: Usuario) {
+
+        for (usuario in listaUsuario) {
+            if (_inscritos.containsKey(usuario.cpf)) {
+                println("O/A Aluno(a): ${usuario.name} ja possui matricula nesta formação")
+            } else {
+                _inscritos[usuario.cpf] = usuario.name
+                println("Matricula Feita !!!!!")
+            }
+        }
+    }
+
+    fun mostrarAlunosMatriculados() {
+        when (inscritos.isNotEmpty()) {
+            true -> {
+                println("Alunos Matriculados: ${inscritos.values}")
+            }
+
+            false -> println("Nenhum Aluno Matriculado")
+        }
     }
 }
 
+
 fun main() {
-    TODO("Analise as classes modeladas para este domínio de aplicação e pense em formas de evoluí-las.")
-    TODO("Simule alguns cenários de teste. Para isso, crie alguns objetos usando as classes em questão.")
+
+    val timeDuration = { nivel: Nivel ->
+        when (nivel.name) {
+            "BASICO" -> 60
+            "INTERMEDIARIO" -> 120
+            else -> 180
+        }
+    }
+
+    val formacao = Formacao(
+        "Android Developer",
+        listOf(
+            ConteudoEducacional(
+                "Linguagem de Programação Kotlin",
+                Nivel.BASICO,
+                timeDuration(Nivel.BASICO)
+            ),
+            ConteudoEducacional(
+                "Fundamentos de Desenvolvimento Mobile Nativo Para Android",
+                Nivel.BASICO,
+                timeDuration(Nivel.BASICO)
+            ),
+            ConteudoEducacional(
+                "Noções Básicas do Android com Kotlin",
+                Nivel.INTERMEDIARIO,
+                timeDuration(Nivel.INTERMEDIARIO)
+            ),
+            ConteudoEducacional(
+                "Dominando o Android Jetpack",
+                Nivel.AVANCADO,
+                timeDuration(Nivel.AVANCADO)
+            )
+        )
+    )
+
+    formacao.mostrarAlunosMatriculados()
+    println("/////////////////")
+
+    formacao.matricularAlunos(
+        Usuario(
+            "Felipe",
+            "111.111.111.11"
+        ),
+        Usuario(
+            "Ana",
+            "222.222.222.22"
+        ),
+        Usuario(
+            "Carlos",
+            "222.222.222.22"
+        ),
+        Usuario(
+            "Roberto",
+            "444.444.444.44"
+        ),
+        Usuario(
+            "Juliana",
+            "555.555.555.55"
+        ),
+        Usuario(
+            "Nilton",
+            "111.111.111.11"
+        )
+    )
+    println("/////////////////")
+
+    formacao.mostrarAlunosMatriculados()
 }
